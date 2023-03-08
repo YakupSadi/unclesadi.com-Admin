9c6cacb60d0fc40b252c9e72073bb35fac2dbbf1
Yakup Sadi <sadiyakupsadi@hotmail.com>
2023-03-08 21:55:16 +0300

    8 Mar 2023

---
+++ src/components/index/content.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    props: [
+        'id',
+        'title',
+        'content'
+    ]
+}
+</script>
+
+<template>
+    <div class="content">
+        {{ id }}
+        {{ title }}
+        {{ content }}
+    </div>
+</template>
+
+<style scoped>
+    .content {
+        gap: 1rem;
+        color: #fff;
+        display: grid;
+        margin-bottom: 1rem;
+        grid-auto-columns: 1fr;
+        grid-template-columns: repeat(4, 1fr);
+    }
+</style>
--- src/views/ContentView.vue
+++ src/views/ContentView.vue
@@ -1,6 +1,66 @@
 <script>
 import axios from 'axios'
 import { mapMutations } from 'vuex'
+import Content from '../components/index/content.vue'
+
+export default {
+  components: {
+    Content
+  },
+  data() {
+    return {
+      id      : null,
+      title   : null,
+      content : null
+    }
+  },
+  mounted() {
+    this.isValid(),
+    this.getAllContent()
+  },
+  methods: {
+    ...mapMutations(['isValid']),
+    getAllContent() {
+      axios.get('http://localhost:4000/api/v1/content')
+      .then((res) => {
+        this.id      = res.data.data.map(item => item._id)
+        this.title   = res.data.data[0].title
+        this.content = res.data.data[0].data
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
+
+<template>
+  <main class="main">
+    <Content 
+      v-for="(id, index) in id"
+        :key     = "index"
+        :id      = "id"
+        :title   ="title"
+        :content = "content"
+      />
+  </main>
+</template>
+
+<style scoped>
+  .main {
+    color: #fff;
+    max-width: 100rem;
+    padding: 6rem 2rem 2rem;
+  }
+</style>
+
+
+
+<!-- 
+<script>
+import axios from 'axios'
+import { mapMutations } from 'vuex'
 
 export default {
   data() {
@@ -44,3 +104,4 @@ export default {
   }
 
 </style>
+ -->
