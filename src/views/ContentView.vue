ec9dd02a30e5bc92a60124e6937dac5776a4f8e3
Yakup Sadi <sadiyakupsadi@hotmail.com>
2023-03-25 21:53:03 +0300

    25 Mar 2023

--- src/views/ContentView.vue
+++ src/views/ContentView.vue
@@ -41,6 +41,19 @@ export default {
 
 <template>
     <main class="main">
+        <div class="search">
+            <div class="search_input">
+                <input type="text" placeholder="Search Content..." required>
+            </div>
+
+            <div class="select_category">
+                <select>
+                    <option value="">All</option>
+                    <option value="">JavaScript</option>
+                </select>
+            </div>
+        </div>  
+
         <Content 
             v-for  ="(item, index) in id"
             :key   = "index"
@@ -60,6 +73,34 @@ export default {
         flex-direction: column;
         padding: 6rem 2rem 2rem;
         justify-content: center;
+    }
+
+    .main > .search {
+        width: 100%;
+        display: flex;
+        margin-bottom: 1rem;
+        justify-content: center;
+    }
+
+    .main > .search > .search_input {
+        width: 40rem;
+        border-left: 3px solid #fff;
+    }
+    .main > .search > .search_input > input {
+        font-size: 1.2rem;
+        padding: .5rem 1rem;
+    }
+    .main > .search > .search_input > input:focus,
+    .main > .search > .select_category > select:focus {
+        outline: none;
+    }
+
+    .main > .search > .select_category > select {
+        height: 100%;
+        width: fit-content;
+        appearance: none;
+        font-size: 1.2rem;
+        padding: .5rem 1rem;
     }
 
     /**/
